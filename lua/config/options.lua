9d1efc42c51332eb38d192a925e06be5f393baf0
Pasi Bergman <pasi.bergman@hiq.fi>
2025-06-16 20:21:30 +0300

    Use render-markdown.nvim instead of markdown-preview.nvim. Add mini.files.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,12 +2,26 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
---
-local opt = vim.opt
-
 -- help conceallevel
--- opt.conceallevel = 0
-opt.conceallevel = 3
+-- vim.opt.conceallevel = 0
+-- vim.opt.conceallevel = 3
 
 -- Disable **all animations**
-vim.g.snacks_animate = false
+-- vim.g.snacks_animate = false
+
+-- Set cursor shape in different modes. See :help guicursor
+-- n  = normal mode
+-- v  = visual mode
+-- sm = showmatch in Insert mode
+-- i  = Insert mode
+-- ve = Visual mode with 'selection' "exclusive"
+-- r  = Replace mode
+-- cr = Command-line Replace mode
+-- o  = Operator-pending mode
+--
+-- See lua/plugin/theme.lua for color information
+--
+vim.opt.guicursor = "n-v-c-sm:block-bCursor,i-ve:ver25-iCursor,r-cr-o:hor20-hCursor"
+
+-- Disable conceal in markdown files
+-- See
---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "echasnovski/mini.files",
+    opts = {
+      windows = {
+        preview = false,
+        width_focus = 40,
+        width_preview = 60,
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        use_as_default_explorer = false,
+      },
+    },
+  },
+}
