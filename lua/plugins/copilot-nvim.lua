dd77fef658825ee4567d990b5c7b16cd060d7256
Pasi Bergman <pasi.bergman@hiq.fi>
2026-05-04 16:22:39 +0700

    Remove Github Copilot model setting from configuration. Remove lazy-lock.json

--- lua/plugins/copilot-nvim.lua
+++ lua/plugins/copilot-nvim.lua
@@ -3,7 +3,7 @@ return {
     "zbirenbaum/copilot.lua",
     opts = {
       auth_provider_url = "https://hiqfi.ghe.com/",
-      copilot_model = "Claude Opus 4.6",
+      -- copilot_model = "",
     },
   },
 }
